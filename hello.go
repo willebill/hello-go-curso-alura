80b2958fd46defc70afcb595eb9584f74621b51c
Wille Matheus <wille0matheus@gmail.com>
2025-05-06 20:47:04 -0300

    Adicionando o Scan para guardar o valor de uma variável digitada no teclado.
    Changes to be committed:
    	modified:   hello.go

--- hello.go
+++ hello.go
@@ -2,16 +2,20 @@ package main
 
 import (
 	"fmt"
-	"reflect"
 )
 
 func main() {
-	nome := "Douglas"
+	nome := "Wille Matheus"
 	versao := 1.1
-	idade := 24
 
-	fmt.Println("Olá, sr.", nome, "sua idade é", idade)
+	fmt.Println("Olá, sr.", nome)
 	fmt.Println("Este programa está na versão", versao)
 
-	fmt.Println("O tipo da variável nome é", reflect.TypeOf(nome))
+	fmt.Println("1- Iniciar Monitoramento")
+	fmt.Println("2- Exibir Logs")
+	fmt.Println("0- Sair do Programa")
+
+	var comando int
+	fmt.Scan(&comando)
+	fmt.Println("O comando escolhido foi", comando)
 }
