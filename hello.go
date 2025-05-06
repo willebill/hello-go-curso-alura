63e1c92f8f579d64197f586641d84e64d1473109
Wille Matheus <wille0matheus@gmail.com>
2025-05-06 20:26:15 -0300

    Aprendendo sobre declarações de variáveis em go. Aprendendo que quando coloca o ":=" não é necessário utilizar o "var" e nem o tipo da variável.
    Changes to be committed:
    	modified:   hello.go

--- hello.go
+++ hello.go
@@ -1,8 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
-	fmt.Println("Olá Mundo")
-	fmt.Println("Meu primeiro programa em Go!")
+	nome := "Douglas"
+	versao := 1.1
+	idade := 24
+
+	fmt.Println("Olá, sr.", nome, "sua idade é", idade)
+	fmt.Println("Este programa está na versão", versao)
+
+	fmt.Println("O tipo da variável nome é", reflect.TypeOf(nome))
 }
