3a00eb25327b11008f54992c62e33889511105c2
Wille Matheus <wille0matheus@gmail.com>
2025-05-08 21:28:04 -0300

    Adicionando Ifs. Em golang a condicao do if sempre precisa retornar true ou false e não precisa de parênteses.
    Changes to be committed:
    	modified:   hello.go

--- hello.go
+++ hello.go
@@ -18,4 +18,14 @@ func main() {
 	var comando int
 	fmt.Scan(&comando)
 	fmt.Println("O comando escolhido foi", comando)
+
+	if comando == 1 {
+		fmt.Println("Monitorando...")
+	} else if comando == 2 {
+		fmt.Println("Exibindo Logs...")
+	} else if comando == 0 {
+		fmt.Println("Saindo do programa...")
+	} else {
+		fmt.Println("Não conheço este comando")
+	}
 }
