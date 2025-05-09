17e1b662666077caf9bebac9c4518c42a49d4f9b
Wille Matheus <wille0matheus@gmail.com>
2025-05-08 22:26:23 -0300

    Criando funções e organizando o código
    Changes to be committed:
    	modified:   hello.go

--- hello.go
+++ hello.go
@@ -2,22 +2,14 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	nome := "Wille Matheus"
-	versao := 1.1
 
-	fmt.Println("Olá, sr.", nome)
-	fmt.Println("Este programa está na versão", versao)
-
-	fmt.Println("1- Iniciar Monitoramento")
-	fmt.Println("2- Exibir Logs")
-	fmt.Println("0- Sair do Programa")
-
-	var comando int
-	fmt.Scan(&comando)
-	fmt.Println("O comando escolhido foi", comando)
+	exibeIntroducao()
+	exibeMenu()
+	comando := leComando()
 
 	switch comando {
 	case 1:
@@ -26,7 +18,30 @@ func main() {
 		fmt.Println("Exibindo Logs...")
 	case 0:
 		fmt.Println("Saindo do programa...")
+		os.Exit(0)
 	default:
 		fmt.Println("Não conheço este comando")
+		os.Exit(-1)
 	}
+}
+
+func exibeIntroducao() {
+	nome := "Wille"
+	versao := 1.1
+	fmt.Println("Olá, sr.", nome)
+	fmt.Println("Este programa está na versão", versao)
+}
+
+func exibeMenu() {
+	fmt.Println("1- Iniciar Monitoramento")
+	fmt.Println("2- Exibir Logs")
+	fmt.Println("0- Sair do Programa")
+}
+
+func leComando() int {
+	var comandoLido int
+	fmt.Scan(&comandoLido)
+	fmt.Println("O comando escolhido foi", comandoLido)
+
+	return comandoLido
 }
