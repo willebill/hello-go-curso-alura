f4285110b2f66d76b0557e76b2f7760986ed2ea8
Wille Matheus <wille0matheus@gmail.com>
2025-05-08 21:43:08 -0300

    Trocando ifs por switch
    Changes to be committed:
    	modified:   hello.go

--- hello.go
+++ hello.go
@@ -19,13 +19,14 @@ func main() {
 	fmt.Scan(&comando)
 	fmt.Println("O comando escolhido foi", comando)
 
-	if comando == 1 {
+	switch comando {
+	case 1:
 		fmt.Println("Monitorando...")
-	} else if comando == 2 {
+	case 2:
 		fmt.Println("Exibindo Logs...")
-	} else if comando == 0 {
+	case 0:
 		fmt.Println("Saindo do programa...")
-	} else {
+	default:
 		fmt.Println("Não conheço este comando")
 	}
 }
