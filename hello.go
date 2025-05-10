a0fcb874e02c96fff95b9cc2b946fe7427dc4c01
Wille Matheus <wille0matheus@gmail.com>
2025-05-10 11:04:43 -0300

    adicionando o pacote http; entendendo como funcionam as funções que possuem múltiplos retornos;
    Changes to be committed:
    	modified:   hello.go

--- hello.go
+++ hello.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 )
 
@@ -13,7 +14,7 @@ func main() {
 
 	switch comando {
 	case 1:
-		fmt.Println("Monitorando...")
+		iniciarMonitoramento()
 	case 2:
 		fmt.Println("Exibindo Logs...")
 	case 0:
@@ -44,4 +45,11 @@ func leComando() int {
 	fmt.Println("O comando escolhido foi", comandoLido)
 
 	return comandoLido
+}
+
+func iniciarMonitoramento() {
+	fmt.Println("Monitorando...")
+	site := "https://www.alura.com.br/"
+	resp, _ := http.Get(site)
+	fmt.Println(resp)
 }
